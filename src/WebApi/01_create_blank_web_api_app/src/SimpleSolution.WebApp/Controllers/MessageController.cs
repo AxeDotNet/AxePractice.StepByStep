e420c82193f72e104e0c567241dbcdcba7751159
Liu Xia <lxconan@gmail.com>
2017-06-26 17:13:30 +0800

    [liuxia] Add first step to create blank webapi app.

---
+++ src/WebApi/01_create_blank_web_api_app/src/SimpleSolution.WebApp/Controllers/MessageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp.Controllers
+{
+    public class MessageController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new {message = "Hello"});
+        }
+    }
+}
