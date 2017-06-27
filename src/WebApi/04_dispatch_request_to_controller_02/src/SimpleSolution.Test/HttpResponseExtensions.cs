52d132382bc472ec96fbb7667691b77a3cd21d01
Liu Xia <lxconan@gmail.com>
2017-06-27 18:28:00 +0800

    [liuxia] Add uri routing practice.

---
+++ src/WebApi/04_dispatch_request_to_controller_02/src/SimpleSolution.Test/HttpResponseExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+
+namespace SimpleSolution.Test
+{
+    static class HttpResponseExtensions
+    {
+        public static Task<T> ReadAsAsync<T>(
+            this HttpContent content, 
+            T template, 
+            params MediaTypeFormatter[] formatters)
+        {
+            return content.ReadAsAsync<T>(formatters);
+        }
+    }
+}
