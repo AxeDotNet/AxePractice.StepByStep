0f3842ae094d14c5bee8e5485a85d499a7180d18
Liu Xia <lxconan@gmail.com>
2017-06-28 18:06:44 +0800

    [liuxia] Add dispatching practices.

---
+++ src/WebApi/practices/01_website_and_request_dispatching/src/SimpleSolution.Test/HttpMethodDispatchingFacts.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SimpleSolution.Test
+{
+    public class HttpMethodDispatchingFacts : ResourceTestBase
+    {
+        [Theory]
+        [InlineData("GET")]
+        [InlineData("PUT")]
+        [InlineData("POST")]
+        [InlineData("DELETE")]
+        public async Task should_get_ok_for_all_methods_for_resource(string method)
+        {
+            HttpResponseMessage response = await Client.SendAsync(
+                new HttpRequestMessage(new HttpMethod(method), "resource"));
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task should_get_ok_for_getting_users()
+        {
+            HttpResponseMessage response = await Client.GetAsync("users");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("PUT")]
+        [InlineData("POST")]
+        [InlineData("DELETE")]
+        public async Task should_avoid_using_short_cuts_for_users(string method)
+        {
+            HttpResponseMessage response = await Client.SendAsync(
+                new HttpRequestMessage(new HttpMethod(method), "users"));
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
---
+++ src/WebApi/practices/01_website_and_request_dispatching/src/SimpleSolution.WebApp/Bootstrapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http;
+
+namespace SimpleSolution.WebApp
+{
+    public class Bootstrapper
+    {
+        public static void Init(HttpConfiguration configuration)
+        {
+        }
+    }
+}
