4b4b058ed0c90777e0a19d29ae4c3cac75917573
Blink <blink@thoughtworks.com>
2017-07-03 17:49:30 +0800

    [xiaowei] request dispatching exam

--- src/WebApi/practices/02_website_and_request_dispatching_02/src/SimpleSolution.WebApp/Bootstrapper.cs
+++ src/WebApi/practices/02_website_and_request_dispatching_02/src/SimpleSolution.WebApp/Bootstrapper.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace SimpleSolution.WebApp
 {
@@ -10,6 +12,31 @@ namespace SimpleSolution.WebApp
             // of our test. So I have create an extension method called
             // Request.Text(HttpStatusCode, string) to help you generating
             // a textual response.
+            var routes = configuration.Routes;
+            routes.MapHttpRoute("get user dependents by id",
+                "users/{id}/dependents",
+                new {controller = "Users", action = "GetDependentsById"},
+                new {httpMehtod = new HttpMethodConstraint(HttpMethod.Get), id = @"\d+"});
+
+            routes.MapHttpRoute("get user dependents",
+                "users/dependents",
+                new {controller = "Users", action = "GetDependents"},
+                new {httpMehtod = new HttpMethodConstraint(HttpMethod.Get)});
+
+            routes.MapHttpRoute("get user by id",
+                "users/{id}",
+                new {controller = "Users", action = "GetById"},
+                new {httpMehtod = new HttpMethodConstraint(HttpMethod.Get), id = @"\d+"});
+
+            routes.MapHttpRoute("get user by name",
+                "users",
+                new {controller = "Users", action = "GetByName"},
+                new {httpMehtod = new HttpMethodConstraint(HttpMethod.Get)});
+
+            routes.MapHttpRoute("update user by id",
+                "users/{id}",
+                new {controller = "Users", action = "Update"},
+                new {httpMehtod = new HttpMethodConstraint(HttpMethod.Put), id = @"\d+"});
         }
     }
 }
---
+++ src/WebApi/practices/02_website_and_request_dispatching_02/src/SimpleSolution.WebApp/Controller/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp.Controller
+{
+    public class UsersController: ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetById(int id)
+        {
+            return Request.Text(HttpStatusCode.OK, $"user(id={id}) found");
+        }
+
+        [HttpPut]
+        public HttpResponseMessage Update(int id, string name)
+        {
+            return Request.Text(HttpStatusCode.OK, $"user(id={id})'s name updated to {name}");
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetByName(string name)
+        {
+            return Request.Text(HttpStatusCode.OK, $"user(name={name}) found");
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetDependents()
+        {
+            return Request.Text(HttpStatusCode.OK, "This will get all users's dependents");
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetDependentsById(int id)
+        {
+            return Request.Text(HttpStatusCode.OK, $"This will get user(id={id})'s dependents");
+        }
+    }
+}
