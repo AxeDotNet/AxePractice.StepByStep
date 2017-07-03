d4b63b25e0e043f071f07fb2128fc74af2eb00a6
Liu Xia <lxconan@gmail.com>
2017-07-03 16:58:38 +0800

    [liuxia] add web app routing examination.

---
+++ src/WebApi/practices/03_website_and_routing_examination/src/SimpleSolution.WebApp/Bootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace SimpleSolution.WebApp
+{
+    public class Bootstrapper
+    {
+        public static void Init(HttpConfiguration config)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
