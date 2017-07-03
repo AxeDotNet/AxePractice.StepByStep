dca301966c5245cffb487fc88290fde11c82fae4
Blink <blink@thoughtworks.com>
2017-07-03 09:05:58 +0800

    [xiaowei] simple dispatch

---
+++ src/WebApi/practices/01_website_and_request_dispatching/src/SimpleSolution.WebApp/Controller/ResourceController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp.Controller
+{
+    public class ResourceController : ApiController
+    {
+        [AcceptVerbs("GET", "PUT", "POST", "DELETE")]
+        public HttpResponseMessage Resource()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ src/WebApi/practices/01_website_and_request_dispatching/src/SimpleSolution.WebApp/Controller/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp.Controller
+{
+    public class UsersController: ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
