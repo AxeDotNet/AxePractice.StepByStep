5238ce946c8d729e6e62f4b100a3276956e923f3
Liu Xia <lxconan@gmail.com>
2017-07-11 16:12:56 +0800

    [liuxia] Add guideline to http controller type resolver.

--- src/LocalApi/03_invoke_controller_action_from_route/src/LocalApi/DefaultHttpControllerTypeResolver.cs
+++ src/LocalApi/03_invoke_controller_action_from_route/src/LocalApi/DefaultHttpControllerTypeResolver.cs
@@ -7,6 +7,11 @@ namespace LocalApi
     class DefaultHttpControllerTypeResolver : IHttpControllerTypeResolver
     {
         #region Please modify the following code to pass the test
+
+        /*
+         * This class is used to get all types that is non-abstract public controller
+         * from given assemblies.
+         */
         
         public ICollection<Type> GetControllerTypes(IEnumerable<Assembly> assemblies)
         {
