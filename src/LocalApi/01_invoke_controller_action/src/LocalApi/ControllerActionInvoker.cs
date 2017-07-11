1e1fe416f53c0ce1ef3dd1bebd224497d23188b9
Liu Xia <lxconan@gmail.com>
2017-07-11 10:56:06 +0800

    [liuxia] Add guided text for practice 01.

--- src/LocalApi/01_invoke_controller_action/src/LocalApi/ControllerActionInvoker.cs
+++ src/LocalApi/01_invoke_controller_action/src/LocalApi/ControllerActionInvoker.cs
@@ -5,9 +5,27 @@ namespace LocalApi
 {
     static class ControllerActionInvoker
     {
+        #region Please modify the code to pass the test
+
+        /*
+         * Now we will create a framework just like the ASP.NET WebApi. We called
+         * it LocalApi. The core part of the Api framework is generating response.
+         * And the most important type for generating response is the HttpController.
+         * In this practice, we try invoking the specified action of the controller
+         * to generate a response.
+         * 
+         * The class to invoke controller action is called a ControllerActionInvoker,
+         * and the entry point is called InvokeAction. It accepts an actionDescriptor
+         * which contains the instance of the controller (currently we have no idea
+         * where it comes from), the name of the action to invoke. For simplicity,
+         * we assume that all actions contains no parameter.
+         */
+
         public static HttpResponseMessage InvokeAction(ActionDescriptor actionDescriptor)
         {
             throw new NotImplementedException();
         }
+
+        #endregion
     }
 }
