aaa368b03968c575c0a11a195a878a43bb7151de
Liu Xia <lxconan@gmail.com>
2017-07-11 11:29:11 +0800

    [liuxia] Rename method to method constraint.

--- src/LocalApi/02_invoke_controller_action_constraint/src/LocalApi/ControllerActionInvoker.cs
+++ src/LocalApi/02_invoke_controller_action_constraint/src/LocalApi/ControllerActionInvoker.cs
@@ -12,16 +12,33 @@ namespace LocalApi
             MethodInfo method = GetAction(actionDescriptor);
             if (method == null) { return new HttpResponseMessage(HttpStatusCode.NotFound); }
 
-            HttpResponseMessage errorResponse = ProcessConstraint(method, actionDescriptor.Method);
+            HttpResponseMessage errorResponse = ProcessConstraint(method, actionDescriptor.MethodConstraint);
             if (errorResponse != null) { return errorResponse; }
 
             return Execute(actionDescriptor, method);
         }
 
+        #region Please modify the following code to pass the test
+
+        /*
+         * Good job! You have passed the first stage. Now we refactored your code
+         * to make the process clearer. Please refer to InvokeAction method.
+         * 
+         * However, we would like to add a constraint on method invokation -- the
+         * HttpMethod constraint. We assumes that all the action method of 
+         * HttpController should be annotated by attributes who implements
+         * IMethodProvider interface. If there is one attribute matches the constraint,
+         * the you should continue invoking action, or else just forbidden the request.
+         * 
+         * Please implements the logic in ProcessConstraint method.
+         */
+
         static HttpResponseMessage ProcessConstraint(MethodInfo method, HttpMethod methodConstraint)
         {
             return null;
         }
+
+        #endregion
 
         static HttpResponseMessage Execute(ActionDescriptor actionDescriptor, MethodInfo method)
         {
@@ -43,8 +60,7 @@ namespace LocalApi
             Type controllerType = controller.GetType();
             const BindingFlags controllerActionBindingFlags =
                 BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase;
-            MethodInfo method = controllerType.GetMethod(actionName, controllerActionBindingFlags);
-            return method;
+            return controllerType.GetMethod(actionName, controllerActionBindingFlags);
         }
     }
 }
