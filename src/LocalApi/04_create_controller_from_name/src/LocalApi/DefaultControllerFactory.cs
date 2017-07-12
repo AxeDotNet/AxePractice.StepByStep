438df7d66a0a9dab9bf5859dea84c5354816ecab
Liu Xia <lxconan@gmail.com>
2017-07-12 10:29:29 +0800

    [liuxia] Add additional guideline.

--- src/LocalApi/04_create_controller_from_name/src/LocalApi/DefaultControllerFactory.cs
+++ src/LocalApi/04_create_controller_from_name/src/LocalApi/DefaultControllerFactory.cs
@@ -12,6 +12,12 @@ namespace LocalApi
         {
             #region Please modify the following code to pass the test.
 
+            /*
+             * The controller factory will create controller by its name. It will search
+             * form the controllerTypes collection to get the correct controller type,
+             * then create instance from resolver.
+             */
+
             throw new NotImplementedException();
 
             #endregion
