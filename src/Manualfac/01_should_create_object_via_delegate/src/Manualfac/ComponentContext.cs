c763f251b1fc3a2698cc31bb959adf0a7cab5184
Blink <blink@thoughtworks.com>
2017-07-18 19:06:29 +0800

    simple manual fac.

--- src/Manualfac/01_should_create_object_via_delegate/src/Manualfac/ComponentContext.cs
+++ src/Manualfac/01_should_create_object_via_delegate/src/Manualfac/ComponentContext.cs
@@ -1,22 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Manualfac
 {
     public class ComponentContext : IComponentContext
     {
+        readonly Dictionary<Type, Func<IComponentContext, object>> registrations;
+
+        internal ComponentContext(Dictionary<Type, Func<IComponentContext, object>> registrations)
+        {
+            this.registrations = registrations;
+        }
+
         #region Please modify the following code to pass the test
 
         /*
          * A ComponentContext is used to resolve a component. Since the component
          * is created by the ContainerBuilder, it brings all the registration
-         * information. 
-         * 
+         * information.
+         *
          * You can add non-public member functions or member variables as you like.
          */
 
         public object ResolveComponent(Type type)
         {
-            throw new NotImplementedException();
+            if (!registrations.ContainsKey(type))
+            {
+                throw new DependencyResolutionException();
+            }
+
+            return registrations[type].Invoke(this);
         }
 
         #endregion
--- src/Manualfac/01_should_create_object_via_delegate/src/Manualfac/ContainerBuilder.cs
+++ src/Manualfac/01_should_create_object_via_delegate/src/Manualfac/ContainerBuilder.cs
@@ -1,30 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Manualfac
 {
     public class ContainerBuilder
     {
+        readonly Dictionary<Type, Func<IComponentContext, object>> registrations = new Dictionary<Type, Func<IComponentContext, object>>();
+        bool isBuilt;
+
         #region Please modify the following code to pass the test
 
         /*
          * Hello, boys and girls. The container builder is a very good guy to store
          * all the definitions for instantiation as well as lifetime managing. Now,
          * let's forget about lifetime management.
-         * 
+         *
          * ContainerBuilder however, has no idea how to create an instance. So it is
          * the users' job (func). We just store the procedure and call it when needed.
-         * 
+         *
          * You can add non-public member functions or member variables as you like.
          */
 
         public void Register<T>(Func<IComponentContext, T> func)
         {
-            throw new NotImplementedException();
+            registrations[typeof(T)] = c => func(c);
         }
 
         public IComponentContext Build()
         {
-            throw new NotImplementedException();
+            if (isBuilt)
+            {
+                throw new InvalidOperationException();
+            }
+
+            isBuilt = true;
+            return new ComponentContext(registrations);
         }
 
         #endregion
