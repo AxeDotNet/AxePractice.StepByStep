378f6407a9824f3737f50bcd942edd27374d9bd0
Blink <blink@thoughtworks.com>
2017-07-24 20:11:21 +0800

    [xiaowei] Can resolve generic type.

--- src/Manualfac/05_should_handle_register_generic/src/Manualfac/ComponentRegistry.cs
+++ src/Manualfac/05_should_handle_register_generic/src/Manualfac/ComponentRegistry.cs
@@ -35,11 +35,20 @@ namespace Manualfac
 
             /*
              * The following code will go through the source list and find the first source who
-             * can match the service. If no-one can match, then query failed. The function will 
+             * can match the service. If no-one can match, then query failed. The function will
              * return fasle. If we have found one. Then create a concrete component registration
              * and add it to serviceInfos for speed acceleration.
              */
-            throw new NotImplementedException();
+            var registrationFromSource = sources.Select(s => s.RegistrationFor(service)).FirstOrDefault(cr => cr != null);
+            if (registrationFromSource == null)
+            {
+                registration = null;
+                return false;
+            }
+
+            Register(registrationFromSource);
+            registration = registrationFromSource;
+            return true;
 
             #endregion
         }
--- src/Manualfac/05_should_handle_register_generic/src/Manualfac/Sources/OpenGenericRegistrationSource.cs
+++ src/Manualfac/05_should_handle_register_generic/src/Manualfac/Sources/OpenGenericRegistrationSource.cs
@@ -22,12 +22,31 @@ namespace Manualfac.Sources
              * This source has 2 properties: the genericService is used as the key to match the
              * service argument. And the implementorType is used to record the actual type used
              * to create instances.
-             * 
-             * This method will try matching the constructed service to an non-constructed 
+             *
+             * This method will try matching the constructed service to an non-constructed
              * generic type of genericService. If it is matched, then an concrete component
              * registration needed wll be invoked.
              */
-            throw new NotImplementedException();
+            var serviceWithType = service as IServiceWithType;
+            if (serviceWithType == null)
+            {
+                return null;
+            }
+
+            var resolutionType = serviceWithType.ServiceType;
+            if (!resolutionType.IsGenericType || !resolutionType.IsConstructedGenericType)
+            {
+                return null;
+            }
+
+            var openGenericSource = serviceWithType.ChangeType(resolutionType.GetGenericTypeDefinition());
+            if (!openGenericSource.Equals(genericService))
+            {
+                return null;
+            }
+
+            return new ComponentRegistration(service,
+                new ReflectiveActivator(implementorType.MakeGenericType(resolutionType.GetGenericArguments())));
 
             #endregion
         }
