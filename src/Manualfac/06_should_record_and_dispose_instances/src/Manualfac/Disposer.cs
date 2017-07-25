21333752b7a6ee076f6a3fa6b3d0869507728613
Blink <blink@thoughtworks.com>
2017-07-25 09:44:07 +0800

    [xiaowei] Change disposes to Stack to dispose object by order.

--- src/Manualfac/06_should_record_and_dispose_instances/src/Manualfac/Disposer.cs
+++ src/Manualfac/06_should_record_and_dispose_instances/src/Manualfac/Disposer.cs
@@ -6,25 +6,32 @@ namespace Manualfac
 {
     class Disposer : Disposable
     {
-        readonly IList<object> disposes = new List<object>();
-
         #region Please implements the following methods
 
+        readonly Stack<IDisposable> disposes = new Stack<IDisposable>();
         /*
          * The disposer is used for disposing all disposable items added when it is disposed.
          */
 
         public void AddItemsToDispose(object item)
         {
-            disposes.Add(item);
+            if (item is IDisposable)
+            {
+                disposes.Push((IDisposable)item);
+            }
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                disposes.OfType<IDisposable>().ToList().ForEach(o => o.Dispose());
+                while (disposes.Any())
+                {
+                    disposes.Pop().Dispose();
+                }
             }
+
+            base.Dispose(disposing);
         }
 
         #endregion
