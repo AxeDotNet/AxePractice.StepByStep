d74d6d9f18fa3de932252495a9a4917f5274f808
Liu Xia <lxconan@gmail.com>
2017-08-07 16:52:49 +0800

    [liuxia] Create a new example showing how to find gc roots in winDBG.

---
+++ src/GCInPractice/04_examine_roots_using_windbg/src/ExamineRoots/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ExamineRoots
+{
+    class NamedObject
+    {
+        readonly string name;
+
+        public NamedObject(string name)
+        {
+            this.name = name;
+        }
+
+        [SuppressMessage("ReSharper", "ConvertToAutoProperty")]
+        public string Name => name;
+    }
+
+    class NamedObjectWithReference : NamedObject
+    {
+        public object Reference { get; }
+
+        public NamedObjectWithReference(string name, object reference) : base(name)
+        {
+            Reference = reference;
+        }
+    }
+
+    class Program
+    {
+        static readonly NamedObjectWithReference globalRoot = new NamedObjectWithReference(
+            "I am global root", 
+            new NamedObject("I am referenced by global root."));
+
+        static void Main()
+        {
+            object globalRootReference = globalRoot.Reference;
+            var localRoot = new NamedObjectWithReference(
+                "I am local root.",
+                globalRootReference);
+
+            Console.ReadLine();
+
+            GC.KeepAlive(localRoot);
+        }
+    }
+}
