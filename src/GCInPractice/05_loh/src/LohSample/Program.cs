3853bec48c20195bce82ab73e81d0c2b49f67321
Liu Xia <lxconan@gmail.com>
2017-08-08 18:56:13 +0800

    [liuxia] Add large object heap sample.

---
+++ src/GCInPractice/05_loh/src/LohSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LohSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            var largeObject = new byte[900000];
+
+            int largeObjectGeneration = GC.GetGeneration(largeObject);
+            Console.WriteLine($"The generation of largeObject array is: {largeObjectGeneration}.");
+
+            Console.WriteLine("Press enter to exit ...");
+            Console.ReadLine();
+            GC.KeepAlive(largeObject);
+        }
+    }
+}
