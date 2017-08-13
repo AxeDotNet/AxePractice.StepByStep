2833ca82d52535100e765f48736e14800fd88c67
Liu Xia <lxconan@gmail.com>
2017-08-13 20:58:24 +0800

    [liuxia] Finish 05.

---
+++ src/WebApi/05_do_aop_work_using_filter/src/SimpleSolution.WebApp/Services/HttpLogging/HttpLogger.cs
@@ -0,0 +1,10 @@
+namespace SimpleSolution.WebApp.Services.HttpLogging
+{
+    class HttpLogger : IHttpLogger
+    {
+        public void Log(IHttpLog log)
+        {
+            // Use your execllent log library to log the information :-D
+        }
+    }
+}
---
+++ src/WebApi/05_do_aop_work_using_filter/src/SimpleSolution.WebApp/Services/MessageService.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleSolution.WebApp.Services
+{
+    public class MessageService
+    {
+        public string CreateMessage()
+        {
+            return "Hello World!";
+        }
+    }
+}
