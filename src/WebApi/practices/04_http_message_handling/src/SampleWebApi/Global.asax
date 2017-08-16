6a0167f1092a2733602b2d18506f8a87a3812652
Liu Xia <lxconan@gmail.com>
2017-08-16 19:46:05 +0800

    [liuxia] Add message content negotiation test.

---
+++ src/WebApi/practices/04_http_message_handling/src/SampleWebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleWebApi.Global" Language="C#" %>
---
+++ src/WebApi/practices/04_http_message_handling/src/SampleWebApi/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+
+namespace SampleWebApi
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            HttpConfiguration config = GlobalConfiguration.Configuration;
+            InitContainer(config);
+            InitRoute(config);
+            config.EnsureInitialized();
+        }
+
+        void InitRoute(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute("message", "message", new {controller = "Message"});
+        }
+
+        static void InitContainer(HttpConfiguration config)
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterApiControllers(typeof(HttpApplication).Assembly);
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(
+                cb.Build());
+        }
+    }
+}
