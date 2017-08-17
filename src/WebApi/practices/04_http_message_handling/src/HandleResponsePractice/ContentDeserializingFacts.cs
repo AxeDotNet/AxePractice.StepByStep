13df927b4d6c3a1788d9ed1e7771d43b6b5393d2
Liu Xia <lxconan@gmail.com>
2017-08-17 08:51:21 +0800

    [liuxia] Add content deserializing facts.

---
+++ src/WebApi/practices/04_http_message_handling/src/HandleResponsePractice/ContentDeserializingFacts.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HandleResponsePractice
+{
+    public class ContentDeserializingFacts
+    {
+        [Fact]
+        public async Task should_deserialize_json_content()
+        {
+            HttpResponseMessage response = await ClientHelper.Client.GetAsync("complex");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            #region Please modifies the following code to pass the test
+
+            // I just want { id, sizes } here. Please deserialize the content. You cannot
+            // change any code beyond the region.
+
+            #endregion
+
+            object content = null;
+
+            Assert.Equal(2, content.GetPublicDeclaredProperties().Length);
+            Assert.Equal(1, content.GetPropertyValue<int>("id"));
+            Assert.Equal(new [] { "Large", "Medium", "Small" }, content.GetPropertyValue<IEnumerable<string>>("sizes"));
+        }
+
+        [Fact]
+        public async Task should_get_properties_from_deserialized_content()
+        {
+            HttpResponseMessage response = await ClientHelper.Client.GetAsync("complex");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            object content = await response.Content.ReadAsAsync<object>();
+
+            int id = default(int);
+            string name = default(string);
+            IEnumerable<string> sizes = default(IEnumerable<string>);
+
+            #region Please modifies the following code to pass the test
+
+            // I want { id, name, sizes } here. Please get properties from the content. 
+            // You cannot change any code beyond the region.
+            
+            #endregion
+            
+            Assert.Equal(1, id);
+            Assert.Equal("Apple", name);
+            Assert.Equal(new[] { "Large", "Medium", "Small" }, sizes);
+        }
+    }
+}
---
+++ src/WebApi/practices/04_http_message_handling/src/SampleWebApi/ComplexContractController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleWebApi
+{
+    public class ComplexContractController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(
+                HttpStatusCode.OK,
+                new
+                {
+                    Id = 1,
+                    Name = "Apple",
+                    Price = "3.99",
+                    Sizes = new[] {"Large", "Medium", "Small"}
+                });
+        }
+    }
+}
