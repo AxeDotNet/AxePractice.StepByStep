b5e08700e8e0c18c20e728d3906fc9a77f0bb5c2
Liu Xia <lxconan@gmail.com>
2017-08-27 23:59:52 +0800

    [liuxia] Add streaming download practice for webapi.

---
+++ src/WebApi/practices/07_streaming/src/WebApp/WebAppBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using Autofac;
+using Autofac.Integration.WebApi;
+
+namespace WebApp
+{
+    public class WebAppBootstrapper : Bootstrapper.WebApi.Bootstrapper
+    {
+        public WebAppBootstrapper(HttpConfiguration configuration) : base(configuration)
+        {
+        }
+
+        protected override void DoInfrastructureIoCInit(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(typeof(WebAppBootstrapper).Assembly);
+            builder.RegisterType<HttpClient>().SingleInstance();
+        }
+
+        protected override void DoInfrastructureInit(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                "download slow stream",
+                "stream/slow",
+                new {controller = "Stream", action = "GetSlow"},
+                new {httpMethod = new HttpMethodConstraint(HttpMethod.Get)});
+        }
+    }
+}
---
+++ src/WebApi/practices/07_streaming/test/StreamingFacts/ExamineHeaderFacts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StreamingFacts
+{
+    public static class ClientHelper
+    {
+        public static HttpClient Client { get; } = new HttpClient()
+        {
+            BaseAddress = new Uri("http://localhost:49724")
+        };
+    } 
+
+    public class ExamineHeaderFacts
+    {
+        HttpClient Client { get; } = ClientHelper.Client;
+
+        [Fact]
+        public async Task should_check_header_quickly()
+        {
+            string filename = null;
+
+            #region Please implement the following code to pass the test
+
+            /*
+             * You should send a GET request to "stream/slow" and try getting the
+             * content filename from the response in 5 secs. 
+             * 
+             * NOTE: you may have to start the WebApp application on port 49724
+             * before executing the unit test.
+             */
+
+            #endregion
+
+            Assert.Equal("filename.exe", filename);
+        }
+    }
+}
