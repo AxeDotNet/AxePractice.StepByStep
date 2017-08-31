01f3d025010f38a6a39e421c794eaab5dd451e54
Liu Xia <lxconan@gmail.com>
2017-08-31 23:22:55 +0800

    [liuxia] Add guided text for webapi practice 08

--- src/WebApi/practices/08_streaming_upload/src/WebApp/ModifiedWebHostBufferPolicySelector.cs
+++ src/WebApi/practices/08_streaming_upload/src/WebApp/ModifiedWebHostBufferPolicySelector.cs
@@ -9,6 +9,18 @@ namespace WebApp
         {
             #region Please implement the method to return the correct buffer type
 
+            /*
+             * The IHostBufferPolicySelector interface will determine which kind of
+             * request consuming and response returning policy should be used. If
+             * you choose to use buffered request handling, then additional storage
+             * space will be used as soon as the input stream of the request has been
+             * consumingm or no addtional storage will be used to store the request
+             * stream.
+             * 
+             * We are handling large file upload so we should not use streaming if 
+             * the content is something that we do not know (octet-stream).
+             */
+
             throw new NotImplementedException();
 
             #endregion
